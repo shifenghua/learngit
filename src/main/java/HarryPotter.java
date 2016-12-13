a7cbf79dea3538536fb4f5b6120b772375f1cadc
shifenghua <332560547@qq.com>
2016-12-13 20:34:41 +0800

    hp2

--- src/main/java/HarryPotter.java
+++ src/main/java/HarryPotter.java
@@ -2,7 +2,18 @@
  * Created by $ fenghua on 2016/12/13.
  */
 public class HarryPotter {
-    public double printPrice() {
-        return 0;
+    public String printPrice(int num) {
+        if(num==1){
+            return "8 EUR";
+        }else if(num==2){
+            return num*8*0.95+"EUR";
+        }else if(num==3){
+            return num*8*0.9+"EUR";
+        }else if(num==4){
+            return num*8*0.8+"EUR";
+        }else if(num==5){
+            return num*8*0.75+"EUR";
+        }
+        return null;
     }
 }
--- src/test/java/HarryPotterTest.java
+++ src/test/java/HarryPotterTest.java
@@ -1,13 +1,38 @@
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Util.println;
+import static org.testng.Assert.assertEquals;
 
 /**
  * Created by $ fenghua on 2016/12/13.
  */
 public class HarryPotterTest {
 
+    private HarryPotter hp;
+    @BeforeClass
+    public void setUp() throws Exception{
+        hp = new HarryPotter();
+    }
+
     @Test
     public void priceForOne(){
-       HarryPotter hp = new HarryPotter();
-        hp.printPrice();
+        assertEquals("8 EUR", hp.printPrice(1));
+    }
+    @Test
+    public void priceForTwoDiff(){
+        assertEquals("15.2EUR",hp.printPrice(2));
+    }
+    @Test
+    public void priceForThreeDiff(){
+        assertEquals("21.6EUR",hp.printPrice(3));
+    }
+    @Test
+    public void priceForFourDiff(){
+        assertEquals("25.6EUR",hp.printPrice(4));
+    }
+    @Test
+    public void priceForFiveDiff(){
+        assertEquals("30.0EUR",hp.printPrice(5));
     }
 }
